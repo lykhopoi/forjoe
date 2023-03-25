e6c54230232888b1da13efafa3d6aaae7ee550a5
lykhopoi <72532517+lykhopoi@users.noreply.github.com>
2023-03-25 00:12:48 -0600

    Update s7.tf

--- s7.tf
+++ s7.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 resource "aws_vpc" "tf-vpc" {
   cidr_block = "10.0.0.0/16"
 
